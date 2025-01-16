9c03e37211fec1ff6e495cd424b87100e25513fc
Rafid Ahmed <74237112+RafidAhmed17@users.noreply.github.com>
2025-01-16 22:18:34 +0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+
+  $dBServerName = "localhost";
+  $dBUserName = "root";
+  $dBPassword = "";
+  $dBName = "cymed";
+  $conn = mysqli_connect($dBServerName, $dBUserName, $dBPassword, $dBName);
+
+  // Dynamic Database Infinity Free
+  // $dBServerName = "sql113.epizy.com";
+  // $dBUserName = "epiz_32292182";
+  // $dBPassword = "ePeZXFZBDTI";
+  // $dBName = "epiz_32292182_cymed";
+  // $conn = mysqli_connect($dBServerName, $dBUserName, $dBPassword, $dBName);
+  
+ ?>
